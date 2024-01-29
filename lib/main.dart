c85dc31151210b557c3f290ac0d5afc1325424a1
Rajavikas03 <rajavikas00007@gmail.com>
2024-01-29 21:20:18 +0530

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+      //   useMaterial3: true,
+      // ),
       home: MyHomePage(),
     );
   }
@@ -26,6 +29,64 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return
+        // SingleChildScrollView(
+        //   child:
+        Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images.jpeg'), fit: BoxFit.cover),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            // mainAxisSize: MainAxisSize.max,
+            children: [
+              const SizedBox(
+                height: 150,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        ' Alarm ',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 50,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.transparent,
+                child: Text(
+                  'Time',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+    // );
   }
 }
